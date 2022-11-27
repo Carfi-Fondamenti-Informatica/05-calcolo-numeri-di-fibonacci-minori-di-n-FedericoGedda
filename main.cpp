aa6229f9b1545813fa7d860bdcd72e6520856ec9
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-27 21:47:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main() {
    int n,p=0,s=1,c=1;
-   cin >> n;
-   cout << s << endl;
-   while (c++ <=n) {
-      cout << p + s << endl;
-      if (p < s) {
-         p += s;
+   cin>>n;
+   cout<<s<< endl;
+   while (c++<=n) {
+      cout <<p+s<<endl;
+      if (p<s){
+         p+=s;
       } else {
-         s += p;
+         s+=p;
       }
-      if ((p + s) > n) {
+      if((p+s)>n){
         break;
       }
    }
