ebe70976e44f7534e442645baf23e2fc7078203d
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-11-27 21:46:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int n,p=0,s=1,c=1;
+   cin >> n;
+   cout << s << endl;
+   while (c++ <=n) {
+      cout << p + s << endl;
+      if (p < s) {
+         p += s;
+      } else {
+         s += p;
+      }
+      if ((p + s) > n) {
+        break;
+      }
+   }
    return 0;
 }
